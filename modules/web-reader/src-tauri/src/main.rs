f194bd30b3ebe3e56dd0816d272d83e430a11575
neighbourkiller <3319578099@qq.com>
2026-08-29 01:35:22 +0800

    chore(release): bump desktop version to 1.26.8292

--- modules/web-reader/src-tauri/src/main.rs
+++ modules/web-reader/src-tauri/src/main.rs
@@ -2,5 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    std::env::set_var("__NV_DISABLE_EXPLICIT_SYNC", "1");
     legado_reader_lib::run();
 }
